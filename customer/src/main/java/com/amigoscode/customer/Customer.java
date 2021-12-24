6baeb0d69a50dcf095afa605aa1d8de35cfdcad0
Thallys Ronald de Souza <thallys_souza@terceiros.sicredi.com.br>
2021-12-24 15:00:29 -0300

    create a customer servicer

---
+++ customer/src/main/java/com/amigoscode/customer/Customer.java
@@ -0,0 +1,15 @@
+package com.amigoscode.customer;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Customer {
+
+    private Integer id;
+    private String firstName;
+    private String lastname;
+    private String email;
+
+}
