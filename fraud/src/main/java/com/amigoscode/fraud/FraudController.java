4c146ca70df22d53bc80c7bb59e3093ba17269c8
Thallys Ronald de Souza <thallys_souza@terceiros.sicredi.com.br>
2021-12-27 10:14:00 -0300

    fraud-service

---
+++ fraud/src/main/java/com/amigoscode/fraud/FraudController.java
@@ -0,0 +1,23 @@
+package com.amigoscode.fraud;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/fraud-check")
+public class FraudController {
+
+    private final FraudCheckService fraudCheckService;
+
+    public FraudController(FraudCheckService fraudCheckService) {
+        this.fraudCheckService = fraudCheckService;
+    }
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFradster(
+            @PathVariable("customer_id_") Integer customerId){
+        boolean isFraudulentCustomer = fraudCheckService.
+                isFraudulentCustomer(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+
+    }
+}
