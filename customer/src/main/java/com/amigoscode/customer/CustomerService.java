fa018babddaf7823650c73e477b0e5d06907f66e
Thallys Ronald de Souza <thallys_souza@terceiros.sicredi.com.br>
2022-01-23 20:29:46 -0300

    service registry

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
                 .build();
 
         customerRepository.saveAndFlush(customer);
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}",
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class, customer.getId());
 
         if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
